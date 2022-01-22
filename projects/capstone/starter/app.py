070b6312bdefb93d074ea8e036b78aa7bb02cc26
unknown <tokaabdullah97@gmail.com>
2022-01-22 23:23:45 +0200

    post, get, models

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -2,11 +2,77 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_migrate import Migrate 
+from models import setup_db, Actor,Movie
 
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
-  CORS(app)
+  setup_db(app)
+  CORS(app, resources={r"/*": {"origins": "*"}})
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')  
+    response.headers.add('Access-Control-Allow-Headers', 'GET, POST, PATCH, DELETE, OPTION')
+    return response
+
+  @app.route('/home')
+  def home():
+    return jsonify({'success': True}), 200
+
+
+  @app.route('/actors',methods=['GET'])
+  def get_actors():
+
+    actors=Actor.query.all()
+    return jsonify( {"success": True, "actors": [actor.short() for actor in actors]}),200
+
+
+  @app.route('/movies',methods=['GET'])
+  def get_movies():
+    movies=Movie.query.all()
+    return jsonify( {"success": True, "movies": [movie.short() for movie in movies]}),200
+
+  @app.route('/actors',methods=['POST'])
+  def post_actors():
+    body=request.get_json()
+    new_name=body.get('name',None)
+    new_age=body.get('age',None)
+    new_gender=body.get('gender',None)
+    new_movies_id= body.get('movies_id',None)
+
+    try:
+
+      actors=Actor(name=new_name,age=new_age,gender=new_gender,movies_id=new_movies_id)
+      actors.insert()
+        
+       
+      return jsonify({'success':True,'actors': [actors.short()]}),200
+
+    except:
+
+      print(sys.exc_info())
+      abort(422)
+
+  @app.route('/movies',methods=['POST'])
+  def post_movies():
+    body=request.get_json()
+    new_title=body.get('title',None)
+    new_release_date=body.get('release_date',None)
+    #new_actors=body.get('actors',None)
+
+    try:
+
+      movies=Movie(title=new_title,release_date=new_release_date)
+      movies.insert()
+        
+       
+      return jsonify({'success':True,'movies': [movies.short()]}),200
+
+    except:
+
+      print(sys.exc_info())
+      abort(422)
 
   return app
 
--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -1,1 +1,70 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String,Date
+from flask_migrate import Migrate
+import os
 
+
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
+DB_NAME = os.getenv('DB_NAME', 'capstone')
+database_path = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+db = SQLAlchemy()
+
+def setup_db(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+
+
+
+class Movie(db.Model):
+    __tablename__='movies'
+    id = db.Column(db.Integer, primary_key=True)
+    title= db.Column(String,nullable=False)
+    release_date=db.Column(Date,nullable=False)
+    actors = db.relationship ('Actor', backref="movie", lazy=True)
+
+    def update(self):
+
+        db.session.commit()
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def short(self):
+        return { "id": self.id, "title": self.title, "release_date": self.release_date}
+
+
+class Actor(db.Model):
+
+    __tablename__ = 'actors'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(String,nullable=False)
+    age = db.Column(Integer,nullable=False)
+    gender = db.Column(String,nullable=False)
+    movies_id= db.Column(db.Integer, db.ForeignKey('movies.id'))
+ 
+
+    def update(self):
+
+        db.session.commit()
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def short (self):
+        return { "id": self.id, "name": self.name, "age": self.age, "gender": self.gender, "movies_id":self.movies_id}
